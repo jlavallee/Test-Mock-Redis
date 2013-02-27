1646e2795773756f1c7110e7e3223253d2ada109
Jeff Lavallee <jeff@zeroclue.com>
2013-02-26 18:28:44 -0800

    version bumps for 0.09 release

--- lib/Test/Mock/Redis.pm
+++ lib/Test/Mock/Redis.pm
@@ -13,11 +13,11 @@ Test::Mock::Redis - use in place of Redis for unit testing
 
 =head1 VERSION
 
-Version 0.08
+Version 0.09
 
 =cut
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 =head1 SYNOPSIS
 
