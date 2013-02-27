be7ecd7b77e9a9223d06a45a7757cf0e6393f1cd
Jeff Lavallee <jeff@zeroclue.com>
2013-02-26 22:00:29 -0800

    link to http://dev.perl.org/licenses/ in POD

--- lib/Test/Mock/Redis.pm
+++ lib/Test/Mock/Redis.pm
@@ -1075,7 +1075,7 @@ This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
 by the Free Software Foundation; or the Artistic License.
 
-See http://dev.perl.org/licenses/ for more information.
+See L<http://dev.perl.org/licenses/> for more information.
 
 
 =cut
